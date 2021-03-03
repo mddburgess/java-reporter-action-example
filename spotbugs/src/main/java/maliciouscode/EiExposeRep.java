cc8f45f9e9c7dd3d29cfa9207d85d3b64138a7cd
Mike Burgess <mddburgess@users.noreply.github.com>
2021-03-03 15:08:15 -0800

    EI_EXPOSE_REP, EI_EXPOSE_REP2

---
+++ spotbugs/src/main/java/maliciouscode/EiExposeRep.java
@@ -0,0 +1,25 @@
+package maliciouscode;
+
+import java.util.List;
+
+public class EiExposeRep {
+    
+    private String[] arrayVar;
+    private List<String> listVar;
+
+    public String[] getArrayVar() {
+        return arrayVar;
+    }
+
+    public void setArrayVar(String[] arrayVar) {
+        this.arrayVar = arrayVar;
+    }
+
+    public List<String> getListVar() {
+        return listVar;
+    }
+
+    public void setListVar(List<String> listVar) {
+        this.listVar = listVar;
+    }
+}
